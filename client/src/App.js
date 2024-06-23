91fdfb28f5fbb4d440022a4a79cafd933afbf6fa
Sheethal <sh33thal24@gmail.com>
2024-06-23 18:32:58 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { darkTheme } from "./utils/Theme";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 
 const Container = styled.div`
@@ -31,6 +32,7 @@ function App() {
       <Container>
         <Wrapper>
         <BrowserRouter>
+        <Navbar/>
             <Routes>
               <Route path="/" element={<Home />} exact />
               <Route path="/post" element={<CreatePost />} exact />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  flex: 1;
+  background: ${({ theme }) => theme.navbar};
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: bold;
+  font-size: 22px;
+  padding: 14px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  @media only screen and (max-width: 600px) {
+    padding: 10px 12px;
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      Navbar
+    </Container>
+  );
+};
+
+export default Navbar;
