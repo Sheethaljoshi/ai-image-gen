1bb174aab4d8e864556e05f667d8f89f6e2ec8eb
Sheethal <sh33thal24@gmail.com>
2024-06-23 18:40:01 +0530

    finished navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import { AddRounded, ExploreRounded } from "@mui/icons-material";
+import Button from "./button";
 
 const Container = styled.div`
   flex: 1;
@@ -19,10 +21,38 @@ const Container = styled.div`
 `;
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname.split("/");
   return (
     <Container>
-      genAI
-      <Button text= ""
+      GenAI
+      {path[1] === "post" ? (
+        <Button
+          onClick={() => navigate("/")}
+          text="Explore Posts"
+          leftIcon={
+            <ExploreRounded
+              style={{
+                fontSize: "18px",
+              }}
+            />
+          }
+          type="secondary"
+        />
+      ) : (
+        <Button
+          onClick={() => navigate("/post")}
+          text="Create new post"
+          leftIcon={
+            <AddRounded
+              style={{
+                fontSize: "18px",
+              }}
+            />
+          }
+        />
+      )}
     </Container>
   );
 };
